01a69a3f44debd2a9a6abdfe5733904c79f7fa9d
kamdem Brice <kamdembrice770@gmail.com>
2026-03-21 17:04:55 +0100

    Derniere modification de mon portfolio et puis je deploie

--- script.js
+++ script.js
@@ -7,16 +7,16 @@ document.addEventListener("DOMContentLoaded", function () {
     .type("  Python(Django).")
     .pause(2000)
     .delete(null)
-    .type("  Css(Bootstrap, Tailwind).")
+    .type("  React Native.")
+    .pause(2000)
+    .delete(null)
+    .type("  Css(Bootstrap).")
     .pause(2000)
     .delete(null)
     .type("  Js(React, AlpineJs).")
     .pause(2000)
     .delete(null)
-    .type("Créateur De Solution web.")
-    .pause(2000)
-    .delete(null)
-    .type("Passionné D'innovation Et D'originalité.")
+    .type("site web & app mobile.")
     .pause(2000)
     .delete(null)
     .go();
@@ -26,16 +26,19 @@ document.addEventListener("DOMContentLoaded", function () {
 
 // Bouton retour en haut
 let btnTop = document.getElementById("btnTop");
-window.onscroll = function() {scrollFunction()};
-function scrollFunction() {
-    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-        btnTop.style.display = "block";
+
+window.addEventListener("scroll", function () {
+    if (window.scrollY > 200) {
+        btnTop.style.display = 'block';
     } else {
         btnTop.style.display = "none";
     }
-}
+});
 function topFunction() {
-    window.scrollTo({top: 0, behavior:'smooth'});
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
 }
 
 
@@ -47,23 +50,18 @@ const spinner = document.getElementById("loading-spinner");
 // Gestion du formulaire
 document.getElementById("contact-form").addEventListener("submit", function (e) {
     e.preventDefault();
-    // 🔵 1. Afficher le spinner
     spinner.style.display = "block";
-    // 🔵 2. Désactiver le bouton d’envoi pour éviter les doubles clics
     const submitBtn = form.querySelector("button[type='submit']");
     submitBtn.disabled = true;
     submitBtn.textContent = "Envoi...";
 
     emailjs.sendForm("service_8yvk9cb", "template_a3m5izb", this)
         .then(() => {
-            // ✅ Envoi réussi → cacher le spinner
             spinner.style.display = "none";
             submitBtn.disabled = false;
             submitBtn.textContent = "Envoyer";
-            // ✅ Si l'envoi réussit : afficher le modal
             const successModal = new bootstrap.Modal(document.getElementById('successModal'));
             successModal.show();
-            // Réinitialiser le formulaire
             this.reset();
         })
         .catch((error) => {
